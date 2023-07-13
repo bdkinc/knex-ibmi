fb4018a05633d6ad8dc197adf93694c061f8e8ac
bskimball <brian.kimball@bdkonline.com>
2023-07-13 19:35:29 -0400

    build using tsup

---
+++ dist/index.js
@@ -0,0 +1,131 @@
+"use strict";
+var __create = Object.create;
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __getProtoOf = Object.getPrototypeOf;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __export = (target, all) => {
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: true });
+};
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+  }
+  return to;
+};
+var __toESM = (mod, isNodeMode, target) => (target = mod != null ? __create(__getProtoOf(mod)) : {}, __copyProps(
+  // If the importer is in node compatibility mode or this is not an ESM
+  // file that has been converted to a CommonJS file using a Babel-
+  // compatible transform (i.e. "__esModule" has not been set), then set
+  // "default" to the CommonJS "module.exports" for node compatibility.
+  isNodeMode || !mod || !mod.__esModule ? __defProp(target, "default", { value: mod, enumerable: true }) : target,
+  mod
+));
+var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
+
+// src/index.ts
+var src_exports = {};
+__export(src_exports, {
+  default: () => src_default
+});
+module.exports = __toCommonJS(src_exports);
+var process = __toESM(require("process"));
+var import_knex = require("knex");
+var DB2Client = class extends import_knex.knex.Client {
+  constructor(config = {}) {
+    super(config);
+    this.driverName = "odbc";
+    if (this.driverName && config.connection) {
+      this.initializeDriver();
+      if (!config.pool || config.pool && config.pool.max !== 0) {
+        this.initializePool(config);
+      }
+    }
+  }
+  _driver() {
+    return require("odbc");
+  }
+  wrapIdentifierImpl(value) {
+    return value;
+  }
+  printDebug(message) {
+    if (process.env.DEBUG === "true") {
+      this.logger.debug(message);
+    }
+  }
+  // Get a raw connection, called by the pool manager whenever a new
+  // connection needs to be added to the pool.
+  async acquireRawConnection() {
+    this.printDebug("acquiring raw connection");
+    const connectionConfig = this.config.connection;
+    return await this.driver.connect(
+      this._getConnectionString(connectionConfig)
+    );
+  }
+  // Used to explicitly close a connection, called internally by the pool manager
+  // when a connection times out or the pool is shutdown.
+  async destroyRawConnection(connection) {
+    return await connection.close();
+  }
+  _getConnectionString(connectionConfig = {}) {
+    const connectionStringParams = connectionConfig.connectionStringParams || {};
+    const connectionStringExtension = Object.keys(
+      connectionStringParams
+    ).reduce((result, key) => {
+      const value = connectionStringParams[key];
+      return `${result}${key}=${value};`;
+    }, "");
+    return `${`DRIVER=${connectionConfig.driver};SYSTEM=${connectionConfig.host};HOSTNAME=${connectionConfig.host};PORT=${connectionConfig.port};DATABASE=${connectionConfig.database};UID=${connectionConfig.user};PWD=${connectionConfig.password};`}${connectionStringExtension}`;
+  }
+  // Runs the query on the specified connection, providing the bindings
+  // and any other necessary prep work.
+  async _query(connection, obj) {
+    if (!obj || typeof obj == "string")
+      obj = { sql: obj };
+    const method = (obj.method !== "raw" ? obj.method : obj.sql.split(" ")[0]).toLowerCase();
+    obj.sqlMethod = method;
+    if (method === "select" || method === "first" || method === "pluck") {
+      const rows = await connection.query(obj.sql, obj.bindings);
+      if (rows) {
+        obj.response = { rows, rowCount: rows.length };
+      }
+      return obj;
+    }
+    const statement = await connection.createStatement();
+    await statement.prepare(obj.sql);
+    await statement.bind(obj.bindings);
+    obj.response = await statement.execute();
+    return obj;
+  }
+  processResponse(obj, runner) {
+    if (obj === null)
+      return null;
+    const resp = obj.response;
+    const method = obj.sqlMethod;
+    const { rows } = resp;
+    if (obj.output)
+      return obj.output.call(runner, resp);
+    switch (method) {
+      case "select":
+      case "pluck":
+      case "first": {
+        if (method === "pluck")
+          return rows.map(obj.pluck);
+        return method === "first" ? rows[0] : rows;
+      }
+      case "insert":
+      case "del":
+      case "delete":
+      case "update":
+      case "counter":
+        return resp.rowCount;
+      default:
+        return resp;
+    }
+  }
+};
+var src_default = DB2Client;
---
+++ dist/index.mjs
@@ -0,0 +1,108 @@
+var __require = /* @__PURE__ */ ((x) => typeof require !== "undefined" ? require : typeof Proxy !== "undefined" ? new Proxy(x, {
+  get: (a, b) => (typeof require !== "undefined" ? require : a)[b]
+}) : x)(function(x) {
+  if (typeof require !== "undefined")
+    return require.apply(this, arguments);
+  throw Error('Dynamic require of "' + x + '" is not supported');
+});
+
+// src/index.ts
+import * as process from "process";
+import { knex } from "knex";
+var DB2Client = class extends knex.Client {
+  constructor(config = {}) {
+    super(config);
+    this.driverName = "odbc";
+    if (this.driverName && config.connection) {
+      this.initializeDriver();
+      if (!config.pool || config.pool && config.pool.max !== 0) {
+        this.initializePool(config);
+      }
+    }
+  }
+  _driver() {
+    return __require("odbc");
+  }
+  wrapIdentifierImpl(value) {
+    return value;
+  }
+  printDebug(message) {
+    if (process.env.DEBUG === "true") {
+      this.logger.debug(message);
+    }
+  }
+  // Get a raw connection, called by the pool manager whenever a new
+  // connection needs to be added to the pool.
+  async acquireRawConnection() {
+    this.printDebug("acquiring raw connection");
+    const connectionConfig = this.config.connection;
+    return await this.driver.connect(
+      this._getConnectionString(connectionConfig)
+    );
+  }
+  // Used to explicitly close a connection, called internally by the pool manager
+  // when a connection times out or the pool is shutdown.
+  async destroyRawConnection(connection) {
+    return await connection.close();
+  }
+  _getConnectionString(connectionConfig = {}) {
+    const connectionStringParams = connectionConfig.connectionStringParams || {};
+    const connectionStringExtension = Object.keys(
+      connectionStringParams
+    ).reduce((result, key) => {
+      const value = connectionStringParams[key];
+      return `${result}${key}=${value};`;
+    }, "");
+    return `${`DRIVER=${connectionConfig.driver};SYSTEM=${connectionConfig.host};HOSTNAME=${connectionConfig.host};PORT=${connectionConfig.port};DATABASE=${connectionConfig.database};UID=${connectionConfig.user};PWD=${connectionConfig.password};`}${connectionStringExtension}`;
+  }
+  // Runs the query on the specified connection, providing the bindings
+  // and any other necessary prep work.
+  async _query(connection, obj) {
+    if (!obj || typeof obj == "string")
+      obj = { sql: obj };
+    const method = (obj.method !== "raw" ? obj.method : obj.sql.split(" ")[0]).toLowerCase();
+    obj.sqlMethod = method;
+    if (method === "select" || method === "first" || method === "pluck") {
+      const rows = await connection.query(obj.sql, obj.bindings);
+      if (rows) {
+        obj.response = { rows, rowCount: rows.length };
+      }
+      return obj;
+    }
+    const statement = await connection.createStatement();
+    await statement.prepare(obj.sql);
+    await statement.bind(obj.bindings);
+    obj.response = await statement.execute();
+    return obj;
+  }
+  processResponse(obj, runner) {
+    if (obj === null)
+      return null;
+    const resp = obj.response;
+    const method = obj.sqlMethod;
+    const { rows } = resp;
+    if (obj.output)
+      return obj.output.call(runner, resp);
+    switch (method) {
+      case "select":
+      case "pluck":
+      case "first": {
+        if (method === "pluck")
+          return rows.map(obj.pluck);
+        return method === "first" ? rows[0] : rows;
+      }
+      case "insert":
+      case "del":
+      case "delete":
+      case "update":
+      case "counter":
+        return resp.rowCount;
+      default:
+        return resp;
+    }
+  }
+};
+var src_default = DB2Client;
+export {
+  src_default as default
+};
