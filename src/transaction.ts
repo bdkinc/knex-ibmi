779519e21453239183f9fed48e065ec20324ce59
bskimball <brian.kimball@bdkonline.com>
2023-07-14 06:29:01 -0400

    build using tsup

--- src/transaction.ts
+++
@@ -1,7 +0,0 @@
-import { Knex } from "knex";
-// @ts-ignore
-import Transaction from "knex/lib/execution/transaction";
-
-class TransactionDB2 extends Transaction<Knex.Transaction> {}
-
-export default TransactionDB2;
