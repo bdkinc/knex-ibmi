c86d81984f84bad3d5e641d3f36b721893f72411
bskimball <brian.kimball@bdkonline.com>
2025-02-14 13:36:02 -0500

    update stream reader

--- src/index.ts
+++ src/index.ts
@@ -216,8 +216,6 @@ class DB2Client extends knex.Client {
           fetchSize: options?.fetchSize || 1,
         },
         (error, cursor) => {
-          const printError = this.printError;
-
           if (error) {
             this.printError(JSON.stringify(error, null, 2));
           }
@@ -227,15 +225,17 @@ class DB2Client extends knex.Client {
             read() {
               cursor.fetch((error2: unknown, result: unknown) => {
                 if (error2) {
-                  printError(JSON.stringify(error2, null, 2));
+                  console.log(JSON.stringify(error2, null, 2));
                 }
                 if (!cursor.noData) {
                   this.push(result);
                 } else {
-                  this.push(null)
-                  cursor.close((error3: unknown) => {
+                  cursor.close((error3) => {
                     if (error3) {
-                      printError(JSON.stringify(error3, null, 2));
+                      console.log(JSON.stringify(error3, null, 2));
+                    }
+                    if (result) {
+                      this.push(result);
                     }
                   });
                 }
